22bd7817c4bbfa8a57cf69043d7982a7f0ead01f
Pawel Pery <pawel.pery@scylladb.com>
2025-09-12 12:14:05 +0200

    sync

--- tests/with_manual.rs
+++ tests/with_manual.rs
@@ -1,6 +1,8 @@
 mod proto;
 
 use proto::Proto;
+use proto::Read;
+use proto::Response;
 use sansio::SansIo;
 
 #[test]
@@ -8,4 +10,5 @@ fn manual() {
     let proto = Proto::new();
     let mut sansio = SansIo::new();
     let request = sansio.start(proto.start(sansio.handler()));
+    let request = sansio.handle(Response::Read(Read));
 }
