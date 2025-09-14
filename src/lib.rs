b8b9766e907de3ca442d3462d6f44afddcb48f4d
Pawel Pery <ewienik@post.pl>
2025-09-14 18:52:32 +0200

    Refactor TaskChannel

--- src/lib.rs
+++ src/lib.rs
@@ -10,16 +10,12 @@ use core::task::RawWaker;
 use core::task::RawWakerVTable;
 use core::task::Waker;
 
-enum TaskChannel<Request, Response> {
+#[derive(Default)]
+enum Channel<Request, Response> {
     Tx(Request),
     Rx(Response),
+    #[default]
     None,
-}
-
-impl<Request, Response> Default for TaskChannel<Request, Response> {
-    fn default() -> Self {
-        TaskChannel::None
-    }
 }
 
 pub struct Call<Request, Response> {
@@ -33,15 +29,15 @@ impl<Request: Unpin, Response: Unpin> Future for Call<Request, Response> {
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let waker = cx.waker();
         assert!(ptr::eq(waker.vtable(), &WAKER_VTABLE));
-        let ch = unsafe { &mut *(waker.data() as *mut TaskChannel<Request, Response>) };
+        let ch = unsafe { &mut *(waker.data() as *mut Channel<Request, Response>) };
 
         if self.request.is_none() {
-            let TaskChannel::Rx(response) = mem::replace(ch, TaskChannel::None) else {
+            let Channel::Rx(response) = mem::replace(ch, Channel::None) else {
                 unreachable!();
             };
             Poll::Ready(response)
         } else {
-            *ch = TaskChannel::Tx(self.request.take().unwrap());
+            *ch = Channel::Tx(self.request.take().unwrap());
             Poll::Pending
         }
     }
@@ -55,7 +51,7 @@ pub fn call<Request, Response>(request: Request) -> Call<Request, Response> {
 }
 
 pub struct SansIo<'a, Request, Response, Task> {
-    ch: TaskChannel<Request, Response>,
+    ch: Channel<Request, Response>,
     task: Pin<&'a mut Task>,
 }
 
@@ -65,7 +61,7 @@ where
 {
     pub fn start(task: Pin<&'a mut Task>) -> (Self, Option<Request>) {
         let mut sansio = Self {
-            ch: TaskChannel::default(),
+            ch: Channel::default(),
             task,
         };
         let result = sansio.run_async();
@@ -73,7 +69,7 @@ where
     }
 
     pub fn handle(&mut self, response: Response) -> Option<Request> {
-        self.ch = TaskChannel::Rx(response);
+        self.ch = Channel::Rx(response);
         self.run_async()
     }
 
@@ -83,7 +79,7 @@ where
         match self.task.as_mut().poll(&mut cx) {
             Poll::Ready(_) => None,
             Poll::Pending => {
-                let TaskChannel::Tx(request) = mem::replace(&mut self.ch, TaskChannel::None) else {
+                let Channel::Tx(request) = mem::replace(&mut self.ch, Channel::None) else {
                     unreachable!();
                 };
                 Some(request)
