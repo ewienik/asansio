9ee7ae6153a30f77c9bf58a792f5894e517b389c
Pawel Pery <ewienik@post.pl>
2025-10-06 21:25:24 +0200

    Update docs and remove Cargo.lock from the repository

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # sansio
-Async/await state machine lite framework for sans-io functionality
+Async/await state machine for the sans-io design pattern.
+
+## The Idea
+
+This crate is a experiment for Sans I/O in Rust (see [sans I/O for network
+protocols](https://sans-io.readthedocs.io/) documentation to familiar with the
+concept). Writing network protocol without performing I/O operations means
+creating a state machine. Manually creating a state machine could be a tedious
+process. As Rust async/await concept is an actual state machine with implicit
+states created during compilation, this library is an experiment with using
+async/await to provide such state machine automatically.  Let's check if this
+is good solution to the problem.
+
+## Dependency
+
+It is `no_std` crate without allocations on the heap. It depends only on the
+`core`, no other crates. Only examples uses `std`, `clap` and `tokio` as
+`dev-dependencies`.
 
 ## License
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,77 @@
+//! # asansio
+//!
+//! This library contains the async/await state machine for the sans-io design pattern. See [sans
+//! I/O for network protocols](https://sans-io.readthedocs.io/) documentation to familiar with the
+//! concept.  Writing network protocol without performing I/O operations means creating a state
+//! machine.  Manually creating a state machine could be a tedious process. As Rust async/await
+//! concept is an actual state machine with implicit states created during compilation, this
+//! library is an experiment with using async/await to provide such state machine automatically.
+//! Let's check if this is good solution to the problem.
+//!
+//! This crate could be used also for non network protocol cases, everywhere there is a need for
+//! creating a state machine.
+//!
+//! This is `no_std` crate and it doesn't allocate on the heap.
+//!
+//! ## Usage
+//!
+//! See this simple example:
+//!
+//! ```
+//! # use asansio::Sans;
+//! # use std::pin::pin;
+//! #
+//! struct Request<'a>(&'a [u8]);
+//! struct Response<'a>(&'a [u8]);
+//!
+//! async fn sans_task<'a>(sans: Sans<Request<'a>, Response<'a>>) {
+//!     let mut request_buf = [1u8; 10];
+//!     let response = sans.start(&Request(&request_buf)).await;
+//!     assert_eq!(response.response().unwrap().0, [2; 20]);
+//!
+//!     request_buf.fill(3);
+//!     let response = sans.handle(response, &Request(&request_buf)).await;
+//!     assert_eq!(response.response().unwrap().0, [4; 20]);
+//! }
+//!
+//! fn run() {
+//!     let (sans, io) = asansio::new();
+//!
+//!     let task = pin!(sans_task(sans));
+//!
+//!     let request = io.start(task).unwrap();
+//!     assert_eq!(request.request().unwrap().0, [1; 10]);
+//!
+//!     let mut response_buf = [2; 20];
+//!     let request = io.handle(request, &Response(&response_buf)).unwrap();
+//!     assert_eq!(request.request().unwrap().0, [3; 10]);
+//!
+//!     response_buf.fill(4);
+//!     let request = io.handle(request, &Response(&response_buf)).unwrap();
+//!     assert_eq!(request.request().unwrap().0, [5; 10]);
+//! }
+//!
+//! ```
+//!
+//! This crate divides a problem into two parts. The first `Sans` takes care of the state machine
+//! independent of the I/O and the second `Io` is responsible with I/O communication.  There are
+//! two types to manage them: the [Io] and the [Sans], which are constructed by the [new] function.
+//! These two parts communicate using `Request` and `Respond` types, which are defined by the user
+//! (for real scenarios they could be `enums`).
+//!
+//! `Sans` starts communicating with `Io` using [Sans::start] and providing the initial `Request`;
+//! it returns the [SansResponse] from the `Io`.  `Io` starts sans task by using [Io::start] which
+//! returns [IoRequest] from `Sans`. The later communication is done using [Sans::handle] and
+//! [Io::handle], which consume [SansResponse] and [IoRequest].
+//!
+//! See also more [examples](https://github.com/ewienik/asansio/tree/master/examples).
+//!
+//! ## Safety
+//!
+//! The crate uses `unsafe` parts for preparing a proper `async/await` infrastructure. Safety is
+//! guaranteed by consuming the latest [IoRequest] and [SansResponse] - these handlers store
+//! `Request` and `Response` objects and their lifetime is limited to the adjecent calls.
+
 #![no_std]
 
 use core::marker::PhantomData;
@@ -40,6 +114,9 @@ impl<'a, Request: Unpin, Response: Unpin> Future for SansHandle<'a, Request, Res
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let waker = cx.waker();
         assert!(ptr::eq(waker.vtable(), &WAKER_VTABLE));
+
+        // It is safe as waker is build befor each future handle call and the Channel
+        // is valid between await points.
         let ch = unsafe { &mut *(waker.data() as *mut Channel<Request, Response>) };
 
         if let Some(request) = self.request.take() {
@@ -57,7 +134,7 @@ impl<'a, Request: Unpin, Response: Unpin> Future for SansHandle<'a, Request, Res
     }
 }
 
-/// Manages the Sans part's communication
+/// Manages the Sans part
 pub struct Sans<Request, Response> {
     _request: PhantomData<Request>,
     _response: PhantomData<Response>,
@@ -100,11 +177,13 @@ impl<Response> SansResponse<Response> {
         if self.response.is_null() {
             return None;
         }
+
+        // It is save as SansResponse is used only between two adjacent await points
         Some(unsafe { &*self.response })
     }
 }
 
-/// Manages the Io part's communication
+/// Manages the Io part
 pub struct Io<Request, Response> {
     _request: PhantomData<Request>,
     _response: PhantomData<Response>,
@@ -160,7 +239,9 @@ where
     }
 
     fn run_async<Response>(&mut self, ch: Channel<Request, Response>) {
+        // It is safe as now there is no valid Request waiting (IoRequest was consumed)
         let waker = unsafe { Waker::new(&ch as *const _ as *const (), &WAKER_VTABLE) };
+
         let mut cx = Context::from_waker(&waker);
         self.request = match self.task.as_mut().poll(&mut cx) {
             Poll::Ready(_) => None,
@@ -168,6 +249,9 @@ where
                 let Channel::Tx(request) = ch else {
                     unreachable!();
                 };
+
+                // It is safe as this will be the only one IoRequest and it will be consumed by the
+                // next handle call
                 Some(unsafe { &*request })
             }
         }
